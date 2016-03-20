999114e4abf5cbe23eabf86889ac1c2525e32bcb
saqib1324 <saqib.lums@gmail.com>
2016-03-20 02:24:26 +0000

    upto 5 done.

---
+++ client2/client.py
@@ -0,0 +1,118 @@
+import sys
+import argparse
+import socket
+import os
+import time
+import base64
+import json
+
+def sharefiles(conn,client_dir):
+    filename = "Sharefile.dropbin"
+    if os.path.isfile(os.path.join(client_dir, filename)):
+        with open(filename, "r") as file:
+            for line in file:
+                line_words = line.split()
+                shared_file = line_words[0]
+                if os.path.isfile(os.path.join(client_dir, shared_file)):
+                    for i in range(1,len(line_words)):
+                        user = line_words[i]
+                        send_shared_file(conn,shared_file,user)
+            file.close()
+    # else:
+    #     print ('file not found')
+                   
+def send_msg(conn, msg):
+    serialized = json.dumps(msg).encode('utf-8')
+    conn.send(b"%d\n" % len(serialized))
+    conn.sendall(serialized)
+
+def get_file_list(client_dir):
+    files = os.listdir(client_dir)
+    files = [file for file in files if os.path.isfile(os.path.join(client_dir, file))]
+    file_list = {}
+    for file in files:
+        path = os.path.join(client_dir, file)
+        mtime = os.path.getmtime(path)
+        ctime = os.path.getctime(path)
+        file_list[file] = max(ctime, mtime)
+
+    return file_list
+
+def send_new_file(conn, filename):
+    with open(filename, "rb") as file:
+        data = base64.b64encode(file.read()).decode('utf-8')
+        msg = {
+            'type': 'file_add',
+            'filename': filename,
+            'data': data
+        }
+        send_msg(conn, msg)
+
+def send_shared_file(conn, filename,username):
+    with open(filename, "rb") as file:
+        data = base64.b64encode(file.read()).decode('utf-8')
+        msg = {
+            'type': 'file_share',
+            'filename': filename,
+            'data': data
+        }
+        send_msg(conn, msg)
+        send_msg(conn,username)
+        
+def send_username(conn, username):
+    msg = {
+        'user': username
+    }
+    send_msg(conn, msg)
+
+def send_delete_file(conn, filename):
+    msg = {
+        'type': 'file_delete',
+        'filename': filename
+    }
+    send_msg(conn, msg)
+
+def get_changes(client_dir, last_file_list):
+    file_list = get_file_list(client_dir)
+    changes = {}
+    for filename, mtime in file_list.items():
+        if filename not in last_file_list or last_file_list[filename] < mtime:
+            changes[filename] = 'file_add'
+
+    for filename, time in last_file_list.items():
+        if filename not in file_list:
+            changes[filename] = 'file_delete'
+
+    return (changes, file_list)
+
+def handle_dir_change(conn, changes):
+    for filename, change in changes.items():
+        if change == 'file_add':
+            print('new file added ', filename)
+            send_new_file(conn, filename)
+        elif change == 'file_delete':
+            print('file deleted ', filename)
+            send_delete_file(conn, filename)
+
+def watch_dir(conn, client_dir):
+    last_file_list = {}
+    while True:
+        time.sleep(1)
+        sharefiles(conn,client_dir)
+        changes, last_file_list = get_changes(client_dir, last_file_list)
+        handle_dir_change(conn, changes)
+
+def client(server_addr, server_port,username, client_dir):
+    s = socket.socket()
+    s.connect((server_addr, server_port))
+    send_username(s,username)
+    watch_dir(s, client_dir)
+    s.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("server_addr", help="Address of the server.")
+    parser.add_argument("server_port", type=int, help="Port number the server is listening on.")
+    parser.add_argument("username", help="Username of the user.")
+    args = parser.parse_args()
+    client(args.server_addr, args.server_port, args.username, os.getcwd())
